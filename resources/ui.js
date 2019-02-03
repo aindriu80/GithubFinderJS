c4438e724e4128b575982052f6f284c34fcf4fa7
aindriu80 <aindriu80@gmail.com>
2019-02-03 17:51:31 +0000

    Show Alert message - user not found

--- resources/ui.js
+++ resources/ui.js
@@ -1,20 +1,28 @@
 class UI {
   constructor() {
-    this.profile = document.getElementById('profile');
+    this.profile = document.getElementById("profile");
   }
-
+  // DIsplay profile in UI
   showProfile(user) {
     this.profile.innerHTML = `
     <div class="card card-body mb-3">
     <div class="row">
     <div class="col-md-3">
         <img class="img-fluid mb-2" src="${user.avatar_url}">
-            <a href="${user.html_url}" target="_blank" class="btn btn-primary btn-block mb-4">View Profile</a>
+            <a href="${
+              user.html_url
+            }" target="_blank" class="btn btn-primary btn-block mb-4">View Profile</a>
           </div>
           <div class="col-md-9">
-            <span class="badge badge-primary">Public Repos: ${user.public_repos}</span>
-            <span class="badge badge-secondary">Public Gists: ${user.public_gists}</span>
-            <span class="badge badge-success">Followers: ${user.followers}</span>
+            <span class="badge badge-primary">Public Repos: ${
+              user.public_repos
+            }</span>
+            <span class="badge badge-secondary">Public Gists: ${
+              user.public_gists
+            }</span>
+            <span class="badge badge-success">Followers: ${
+              user.followers
+            }</span>
             <span class="badge badge-info">Following: ${user.following}</span>
             <br><br>
             <ul class="list-group">
@@ -29,5 +37,39 @@ class UI {
 <h3 class="page-heading mb-3">Latest Repos</h3>
       <div id="repos"></div>
     `;
+  }
+  // SHow alert message
+  showAlert(message, className) {
+    // Clear any remaining alerts
+    this.clearAlert();
+    // Create div
+    const div = document.createElement("div");
+    // Add classes
+    div.className = className;
+    // Add text
+    div.appendChild(document.createTextNode(message));
+    // Get parent
+    const container = document.querySelector(".searchContainer");
+    // Get search box
+    const search = document.querySelector(".search");
+    // Insert Alert
+    container.insertBefore(div, search);
+
+    // Timeout after 3 seconds
+    setTimeout(() => {
+      this.clearAlert();
+    }, 3000);
+  }
+
+  // Clear Alert Message
+  clearAlert() {
+    const currentAlert = document.querySelector(".alert");
+    if (currentAlert) {
+      currentAlert.remove();
+    }
+  }
+  // Clear profile
+  clearProfile() {
+    this.profile.innerHTML = "";
   }
 }
