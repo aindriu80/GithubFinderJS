836e342b90613392914798c719fe0fa6fee60e1b
aindriu80 <aindriu80@gmail.com>
2019-02-04 18:58:26 +0000

    Fetch display repos

--- resources/github.js
+++ resources/github.js
@@ -2,6 +2,8 @@ class Github {
   constructor() {
     this.client_id = "a5688b3c7dca03d68e94";
     this.client_secret = "97d432056d74c834661d4befb5e08d5a3fd803fc";
+    this.repos_count = 10;
+    this.repos_sort = "create: asc";
   }
 
   async getUser(user) {
@@ -11,10 +13,20 @@ class Github {
       }&client_secret=${this.client_secret}`
     );
 
+    const reposResponse = await fetch(
+      `https://api.github.com/users/${user}/repos?per_page=${
+        this.repos_count
+      }&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${
+        this.client_secret
+      }`
+    );
+
     const profile = await profileResponse.json();
+    const repos = await reposResponse.json();
 
     return {
-      profile
+      profile,
+      repos
     };
   }
 }
